8033c2cf428d31a6d6fb087c58fb6bde67441e9a
Bruno Henrique <bruno.henriquy@gmail.com>
2023-04-20 12:41:12 -0300

    Challenge 2

---
+++ src/app/api/chats/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../prisma/prisma";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const chatCreated = await prisma.chat.create({
+    data: {
+      message: body.message
+    },
+    select: {
+      id: true,
+      message: true,
+    },
+  });
+
+  return NextResponse.json(chatCreated);
+}
+
+export async function GET(request: NextRequest) {
+  const chats = await prisma.chat.findMany({
+    select: {
+      id: true,
+      message: true
+    },
+  });
+
+  return NextResponse.json(chats);
+}
